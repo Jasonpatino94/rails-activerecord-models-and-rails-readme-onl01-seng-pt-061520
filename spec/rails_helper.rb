380a863790f807a7333a054f8ded9db1bef6407f
jasonpatino94 <jason.patino94@gmail.com>
2020-10-13 22:01:54 -0400

    Done.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,11 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
+describe Post do
+  it 'can be created' do
+    post = Post.create!(title: "My title". description: "The post description")
+    expect(post).to be_valid  
+  end
+end
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
